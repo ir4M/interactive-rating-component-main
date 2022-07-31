f1e5b1954474f0a554da6394abe416cf34fbd6b5
Marius <marius.hoehn@mni.thm.de>
2022-07-31 22:14:44 +0200

    style for thank you page, submit function and dynamic rating rendering

--- static/client.js
+++ static/client.js
@@ -2,18 +2,44 @@
 var ratingPage;
 var thankYouPage;
 var form;
+var btnContainer;
+var btns;
+var valueRating;
+var feedback;
 document.addEventListener('DOMContentLoaded', function () {
     console.log('client.js wird ausgeführt');
     // Variablen initialisieren
     ratingPage = document.querySelector('#rating');
     thankYouPage = document.querySelector('#thankYou');
-    // Eventhandler registrieren
     form = document.querySelector('#formRating');
+    btnContainer = document.querySelector(".btn-container");
+    btns = btnContainer.querySelectorAll("button[type='button']");
+    valueRating = document.querySelector("#valueRating");
+    feedback = document.querySelector("#feedback");
+    console.log("buttons: " + btns.length);
+    // Eventhandler registrieren
     form.addEventListener('submit', submitRating);
+    // Iteriere über alle Buttons und füge die active class, dem aktuell geklickten Button hinzu
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.querySelectorAll(".active");
+            // Wenn es schon eine active class gibt, entferne diese
+            if (current.length > 0) {
+                current[0].className = current[0].className.replace(" active", "");
+            }
+            // Wenn es noch keine active class gibt, füge die active class dem aktuell geklicktem Button hinzu
+            this.className += " active";
+            valueRating.innerText = this.innerText;
+        });
+    }
 });
 function submitRating(event) {
     event.preventDefault();
-    console.log("submitRating wird ausgeführt");
-    ratingPage.style.display = 'none';
-    thankYouPage.style.display = 'block';
+    if (valueRating.innerText != '') {
+        ratingPage.style.display = 'none';
+        thankYouPage.style.display = 'block';
+    }
+    else {
+        feedback.innerText = 'Please choose a rating';
+    }
 }
--- static/client.ts
+++ static/client.ts
@@ -2,6 +2,11 @@
 let ratingPage: HTMLElement;
 let thankYouPage: HTMLElement;
 let form: HTMLFormElement;
+let btnContainer: HTMLElement;
+let btns: NodeListOf<HTMLButtonElement>;
+let valueRating: HTMLSpanElement;
+let feedback: HTMLSpanElement;
+
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('client.js wird ausgeführt');
@@ -9,16 +14,41 @@ document.addEventListener('DOMContentLoaded', () => {
     // Variablen initialisieren
     ratingPage = document.querySelector('#rating');
     thankYouPage = document.querySelector('#thankYou');
+    form = document.querySelector('#formRating');
+    btnContainer = document.querySelector(".btn-container");
+    btns = btnContainer.querySelectorAll("button[type='button']");
+    valueRating = document.querySelector("#valueRating");
+    feedback = document.querySelector("#feedback");
+    console.log("buttons: " + btns.length);
+
 
     // Eventhandler registrieren
-    form = document.querySelector('#formRating');
     form.addEventListener('submit', submitRating);
+
+    // Iteriere über alle Buttons und füge die active class, dem aktuell geklickten Button hinzu
+    for (let i: number = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+            let current = document.querySelectorAll(".active");
+
+            // Wenn es schon eine active class gibt, entferne diese
+            if (current.length > 0) {
+                current[0].className = current[0].className.replace(" active", "");
+            }
+
+            // Wenn es noch keine active class gibt, füge die active class dem aktuell geklicktem Button hinzu
+            this.className += " active";
+            valueRating.innerText = this.innerText;
+        });
+    }
 })
 
 function submitRating(event: Event): void {
     event.preventDefault();
-    console.log("submitRating wird ausgeführt");
-    ratingPage.style.display = 'none';
-    thankYouPage.style.display = 'block';
-}
 
+    if (valueRating.innerText != '') {
+        ratingPage.style.display = 'none';
+        thankYouPage.style.display = 'block';
+    } else {
+        feedback.innerText = 'Please choose a rating';
+    }
+}
